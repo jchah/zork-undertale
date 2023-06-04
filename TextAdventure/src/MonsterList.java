5c6e34eeaab8497000017214dd5ada9791f4bd27
Ryan Kakavand <rkakavand@bayviewglen.ca>
2023-06-04 11:58:18 -0400

    monsterlist addition

---
+++ TextAdventure/src/MonsterList.java
@@ -0,0 +1,12 @@
+public class MonsterList {
+    public static ArrayList<Monster> snowdinMonsters = new ArrayList<>();
+    public static ArrayList<Monster> waterfallMonsters = new ArrayList<>();
+    public static ArrayList<Monster> coreMonsters = new ArrayList<>();
+    public static ArrayList<Monster> ruinsMonsters = new ArrayList<>();
+    
+    static {
+        Monster froggit = new Monster(30, 4, 4, "Froggit");
+
+    }
+        
+}
