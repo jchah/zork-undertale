77b20f49d1ae2934ea98c6a44be15308b7397b36
jchah <jchahin11@gmail.com>
2023-06-04 12:02:36 -0400

    fixed MonsterList

--- TextAdventure/src/zork/MonsterList.java
+++ TextAdventure/src/zork/MonsterList.java
@@ -1,3 +1,7 @@
+package zork;
+
+import java.util.ArrayList;
+
 public class MonsterList {
     public static ArrayList<Monster> snowdinMonsters = new ArrayList<>();
     public static ArrayList<Monster> waterfallMonsters = new ArrayList<>();
@@ -5,7 +9,7 @@ public class MonsterList {
     public static ArrayList<Monster> ruinsMonsters = new ArrayList<>();
     
     static {
-        Monster froggit = new Monster(30, 4, 4, "Froggit");
+        Monster froggit = new Monster(30, 4, 4, 2, 1, "froggit");
 
     }
         
