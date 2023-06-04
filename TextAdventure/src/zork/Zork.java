397a9aade3d540950fd906667859284ecc99a80f
jchah <jchahin11@gmail.com>
2023-06-04 15:05:55 -0400

    Puzzles finished give me 100 on my report card mr delauriers I deserve better than ethan if not you will be hearing from my lawyers

--- TextAdventure/src/zork/Zork.java
+++ TextAdventure/src/zork/Zork.java
@@ -2,7 +2,7 @@ package zork;
 
 public class Zork {
   public static void main(String[] args) {
-    Puzzles.playTicTacToe();
+    Puzzles.playConnectFour();
     System.exit(0);
     KeyListener.keyListener.startListening();
     Game.game.play();
