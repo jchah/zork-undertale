5d02a63d9fa2329507c9025e02b078d66ab0b6f4
Ryan Kakavand <rkakavand@bayviewglen.ca>
2023-06-04 15:10:07 -0400

    added lamp image

--- TextAdventure/src/zork/Zork.java
+++ TextAdventure/src/zork/Zork.java
@@ -2,8 +2,6 @@ package zork;
 
 public class Zork {
   public static void main(String[] args) {
-    Puzzles.playTicTacToe();
-    System.exit(0);
     KeyListener.keyListener.startListening();
     Game.game.play();
     KeyListener.keyListener.stopListening();
